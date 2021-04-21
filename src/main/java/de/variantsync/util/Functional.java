4e9a0a199e0c12cf876e36fd40db197d4d9fddce
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2021-04-21 17:00:04 +0200

    functions to create generic arrays

--- src/main/java/de/variantsync/util/Functional.java
+++ src/main/java/de/variantsync/util/Functional.java
@@ -1,11 +1,25 @@
 package de.variantsync.util;
 
+import de.variantsync.evolution.variant.VariantRevision;
+
+import java.lang.reflect.Array;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.IntFunction;
 import java.util.stream.Collectors;
 
 public class Functional {
-    public static <T, U> List<U> fmap(List<T> a, Function<T, U> f) {
+
+    public static <T, U> List<U> fmap(List<? extends T> a, Function<T, U> f) {
         return a.stream().map(f).collect(Collectors.toList());
+    }
+
+    @SafeVarargs
+    public static <T, U extends T> T[] createArray(U... ts) {
+        return ts;
+    }
+
+    public static <T> IntFunction<T[]> createArray() {
+        return len -> (T[]) new Object[len];
     }
 }
