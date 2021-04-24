34f8e9dfd8e90400d09ccb3ce7e6c65af9616907
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2021-04-24 21:32:24 +0200

    more documentation for lazy + pure

--- src/main/java/de/variantsync/util/Lazy.java
+++ src/main/java/de/variantsync/util/Lazy.java
@@ -6,8 +6,8 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
- * Monad for explizit lazy evaluation.
- * A Lazy<T> represents a computation returning a T.
+ * Monad for explicit lazy evaluation.
+ * A Lazy<A> represents a computation returning a A.
  * Using lazy allows to compose such computations without ever running the actual computation.
  * This way, writing a program is separated from evaluating the program.
  * (This lazy monad is actually the reader monad with an empty environment + a cache.)
@@ -17,41 +17,76 @@ import java.util.function.Supplier;
  * In this project, this means that the interactions with the file system and git repositories will take place at exactly
  * that moment.
  *
- * @param <T> The return type of this lazy computation.
+ * @param <A> The return type of this lazy computation.
  */
-public class Lazy<T> {
-    private final Supplier<? extends T> get;
-    private T val = null;
+public class Lazy<A> {
+    private final Supplier<? extends A> get;
+    private A val = null;
 
-    public Lazy(Supplier<? extends T> get) {
+    private Lazy(A val) {
+        assert val != null;
+        this.val = val;
+        this.get = null;
+    }
+
+    private Lazy(Supplier<? extends A> get) {
         this.get = get;
     }
 
-    public T run() {
+    public static <B> Lazy<B> of(Supplier<? extends B> f) {
+        return new Lazy<>(f);
+    }
+
+    public static <B> Lazy<B> pure(B b) {
+        return new Lazy<>(b);
+    }
+
+    /**
+     * Run the lazy computation and obtain the result.
+     * @return The result of this lazy computation.
+     */
+    public A run() {
         if (val == null) {
             val = get.get();
         }
         return val;
     }
 
-    public <U> Lazy<U> map(Function<? super T, ? extends U> f) {
+    /**
+     * Lazy is a functor.
+     * @param f Function to apply to the result of this lazy when it is computed.
+     * @return Composed lazy that applies f to the result of this lazy after computation.
+     */
+    public <B> Lazy<B> map(Function<? super A, ? extends B> f) {
         return new Lazy<>(() -> f.apply(run()));
     }
 
-    public <U> Lazy<U> bind(Function<? super T, Lazy<U>> f) {
+    /**
+     * Lazy is an applicative functor.
+     * @param lf Lazy that holds a function to apply to this lazy's result after computation (similar to map).
+     * @return Composed lazy that applies the function computed by lf to the result of this lazy after computation.
+     */
+    public <B> Lazy<B> splat(Lazy<Function<? super A, ? extends B>> lf) {
+        return new Lazy<>(() -> lf.run().apply(run()));
+    }
+
+    /**
+     * Lazy is a monad.
+     * Chains the given lazy computation with this one (i.e., applies the given lazy comptuation to the result of this lazy once its computed).
+     * You might replace "bind" with "then" in your mind to see how it works.
+     * @param f A lazy computation to chain to this one.
+     * @return Returns a new lazy computation composed of this and the given lazy.
+     */
+    public <B> Lazy<B> bind(Function<? super A, Lazy<? extends B>> f) {
         // This is the inlined version of `join(map(f))` for performance reasons.
         return new Lazy<>(() -> f.apply(run()).run()); // == join(map(f))
     }
 
-    public static <U> Lazy<U> join(Lazy<Lazy<U>> l) {
+    public static <B> Lazy<B> join(Lazy<Lazy<B>> l) {
         return new Lazy<>(() -> l.run().run());
     }
 
-    public <U> Lazy<Pair<T, U>> and(Lazy<? extends U> other) {
+    public <B> Lazy<Pair<A, B>> and(Lazy<? extends B> other) {
         return new Lazy<>(() -> new Pair<>(run(), other.run()));
-    }
-
-    public static <U> Lazy<U> of(Supplier<? extends U> f) {
-        return new Lazy<>(f);
     }
 }
