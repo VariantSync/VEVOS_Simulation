b7fa202fe021f5bcfa0432924e7521a8b89d3df9
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2021-04-24 22:34:17 +0200

    Unit value

---
+++ src/main/java/de/variantsync/util/Unit.java
@@ -0,0 +1,24 @@
+package de.variantsync.util;
+
+/**
+ * Unit represents a type that has exactly one value (Instance()).
+ */
+public class Unit {
+    private static final Unit instance = new Unit();
+
+    private Unit() {}
+
+    public static Unit Instance() {
+        return instance;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Unit;
+    }
+
+    @Override
+    public String toString() {
+        return "()";
+    }
+}
