56ab8ecb59ccd7e4087dc045784155b4b89d311d
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2021-04-24 21:32:50 +0200

    placeholder class for variability data

---
+++ src/main/java/de/variantsync/subjects/FeatureTraces.java
@@ -0,0 +1,15 @@
+package de.variantsync.subjects;
+
+import de.variantsync.feature.Variant;
+import de.variantsync.repository.ISPLRepository;
+import de.variantsync.repository.IVariantsRepository;
+import de.variantsync.util.NotImplementedException;
+
+public class FeatureTraces {
+    // TODO: Implement Issue #1 somewhere here? Also in generateVariant?
+
+    public void generateVariant(Variant variant, ISPLRepository splRepo, IVariantsRepository variantsRepo) {
+        // TODO: Implement Issue #2 here.
+        throw new NotImplementedException();
+    }
+}
