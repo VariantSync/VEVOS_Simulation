7418a7928b5e40187e5a73013fea298581491f79
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2021-06-11 11:42:26 +0200

    docs for interval

--- src/main/java/de/variantsync/evolution/util/math/Interval.java
+++ src/main/java/de/variantsync/evolution/util/math/Interval.java
@@ -1,6 +1,11 @@
 package de.variantsync.evolution.util.math;
-
+/**
+ * Represents an interval of integers [from, to].
+ */
 public record Interval(int from, int to) {
+    /**
+     * @return True iff i is within this interval.
+     */
     public boolean contains(int i) {
         return from <= i && i <= to;
     }
--- src/main/java/de/variantsync/evolution/util/math/IntervalSet.java
+++ src/main/java/de/variantsync/evolution/util/math/IntervalSet.java
@@ -6,6 +6,9 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * A set of integer intervals.
+ */
 public class IntervalSet implements Monoid<IntervalSet>, Iterable<Interval> {
     private final List<Interval> intervals;
 
@@ -18,10 +21,17 @@ public class IntervalSet implements Monoid<IntervalSet>, Iterable<Interval> {
         intervals.add(interval);
     }
 
+    /**
+     * Add a new interval [a, b] to this set.
+     */
     public void add(int a, int b) {
         intervals.add(new Interval(a, b));
     }
 
+    /**
+     * Check if the given point lies within any interval in this set.
+     * @return True iff the given point is within any interval in this set.
+     */
     public boolean contains(int i) {
         return intervals.stream().anyMatch(interval -> interval.contains(i));
     }
@@ -39,6 +49,10 @@ public class IntervalSet implements Monoid<IntervalSet>, Iterable<Interval> {
         return i;
     }
 
+    /**
+     * Add all intervals in the given set to this IntervalSet.
+     * @param other IntervalSet whose intervals should be added to this interval.
+     */
     public void mappendInline(IntervalSet other) {
         this.intervals.addAll(other.intervals);
     }
