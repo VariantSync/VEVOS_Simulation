3427179e8ab44331f69253a071d856a526add7d8
pmbittner <paul.bittner@uni-ulm.de>
2021-07-21 17:22:01 +0200

    stub for uniform random sampling

---
+++ src/main/java/de/variantsync/evolution/feature/sampling/UniformRandomSampling.java
@@ -0,0 +1,13 @@
+package de.variantsync.evolution.feature.sampling;
+
+import de.ovgu.featureide.fm.core.base.IFeatureModel;
+import de.variantsync.evolution.feature.Sample;
+import de.variantsync.evolution.feature.Sampler;
+import de.variantsync.evolution.util.NotImplementedException;
+
+public class UniformRandomSampling implements Sampler {
+    @Override
+    public Sample sample(IFeatureModel model) {
+        throw new NotImplementedException();
+    }
+}
