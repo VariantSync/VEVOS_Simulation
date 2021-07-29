fbe763f9cd6c0be10d03905fe6265fb220d0f938
pmbittner <paul.bittner@uni-ulm.de>
2021-07-29 09:52:59 +0200

    abstracted GroundTruth

--- src/main/java/de/variantsync/evolution/variability/pc/GroundTruth.java
+++ src/main/java/de/variantsync/evolution/variability/pc/GroundTruth.java
@@ -1,3 +1,11 @@
 package de.variantsync.evolution.variability.pc;
 
-public record GroundTruth(LineBasedAnnotation variantArtefact, BlockMatching matching) { }
+import de.variantsync.evolution.util.functional.Functor;
+
+import java.util.function.Function;
+
+public record GroundTruth<A extends Artefact>(A variantArtefact, BlockMatching matching) {
+    public <B extends Artefact> GroundTruth<B> map(Function<? super A, ? extends B> f) {
+        return new GroundTruth<>(f.apply(variantArtefact), matching);
+    }
+}
