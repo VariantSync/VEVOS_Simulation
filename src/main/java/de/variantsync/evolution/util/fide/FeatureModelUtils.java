47064ace4a82496d68b905722d2b28218190035e
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2021-08-03 13:33:52 +0000

    Fixed loading of FeatureModel from VariabilityModel

--- src/main/java/de/variantsync/evolution/util/fide/FeatureModelUtils.java
+++ src/main/java/de/variantsync/evolution/util/fide/FeatureModelUtils.java
@@ -1,19 +1,11 @@
 package de.variantsync.evolution.util.fide;
 
-import de.ovgu.featureide.fm.core.base.IFeature;
-import de.ovgu.featureide.fm.core.base.IFeatureModel;
-import de.ovgu.featureide.fm.core.base.IFeatureModelElement;
-import de.ovgu.featureide.fm.core.base.IFeatureModelFactory;
+import de.ovgu.featureide.fm.core.base.*;
 import de.ovgu.featureide.fm.core.base.impl.DefaultFeatureModelFactory;
 import de.ovgu.featureide.fm.core.base.impl.FMFactoryManager;
-import de.ovgu.featureide.fm.core.base.impl.FactoryManager;
 import de.ovgu.featureide.fm.core.base.impl.Feature;
-import de.ovgu.featureide.fm.core.io.Problem;
-import de.ovgu.featureide.fm.core.io.ProblemList;
-import de.ovgu.featureide.fm.core.io.dimacs.DIMACSFormat;
 import de.variantsync.evolution.io.Resources;
 import de.variantsync.evolution.util.Logger;
-import de.variantsync.evolution.util.functional.Result;
 import net.ssehub.kernel_haven.variability_model.VariabilityModel;
 
 import java.io.IOException;
@@ -46,18 +38,52 @@ public class FeatureModelUtils {
         return FromOptionalFeatures(Arrays.asList(featureNames));
     }
     
-    public static IFeatureModel FromVariabilityModel(VariabilityModel vm) throws Resources.ResourceIOException {
+    public static IFeatureModel FromVariabilityModel(VariabilityModel vm) throws Resources.ResourceIOException, IOException {
         Logger.debug("Converting VariabilityModel to FeatureModel.");
+        // The DIMACS file is missing the 'CONFIG_' prefix for every feature, while the VariabilityModel and the PCs are
+        // with prefix. We amend the DIMACS file by adding the prefix to each feature.
+        amendDimacs(vm.getConstraintModel().toPath());
+        
+        // Load the initial FeatureModel
         IFeatureModel fm = Resources.Instance().load(IFeatureModel.class, vm.getConstraintModel().toPath());
         Logger.debug("Read FeatureModel from DIMACS");
+        
+        // Add all missing features
         Set<String> featureNames = new HashSet<>(vm.getVariableMap().keySet());
         // We only want to add features that are not part of the model yet
         fm.getFeatures().stream().map(IFeatureModelElement::getName).forEach(featureNames::remove);
-        Logger.debug("Found " + featureNames.size() + " features that should be added.");
         IFeatureModelFactory factory = FMFactoryManager.getInstance().getFactory(fm);
+        IFeature root = fm.getFeature("__Root__");
         // Add all remaining feature from the variability model to the feature model
-        featureNames.stream().map(name -> factory.createFeature(fm, name)).forEach(fm::addFeature);
+        featureNames.stream().map(name -> factory.createFeature(fm, name)).forEach(feature -> {
+                FeatureUtils.addFeature(fm, feature);
+                FeatureUtils.addChild(root, feature);
+        });
         Logger.debug("Added all feature from VariabilityModel to FeatureModel.");
         return fm;
+    }
+
+    private static void amendDimacs(Path pathToFile) throws IOException {
+        final String MEND = "CONFIG_";
+        List<String> lines = Files.readAllLines(pathToFile);
+        List<String> amendedLines = new ArrayList<>(lines.size());
+
+        for (String line : lines) {
+            if (line.trim().startsWith("c")) {
+                String[] parts = line.split("\\s+");
+                if (!parts[2].startsWith(MEND)) {
+                    parts[2] = MEND + parts[2];
+                }
+                StringBuilder sb = new StringBuilder();
+                for (String part : parts) {
+                    sb.append(part);
+                    sb.append(" ");
+                }
+                amendedLines.add(sb.toString().trim());
+            } else {
+                amendedLines.add(line);
+            }
+        }
+        Files.write(pathToFile, amendedLines);
     }
 }
