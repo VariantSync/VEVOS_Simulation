9debcdd9f3a726b189c8134a0569db8bbb86f163
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2023-03-17 16:23:18 +0100

    logging: minor change in log level

--- src/main/java/org/variantsync/vevos/simulation/variability/pc/variantlines/VariantLine.java
+++ src/main/java/org/variantsync/vevos/simulation/variability/pc/variantlines/VariantLine.java
@@ -24,7 +24,7 @@ public record VariantLine(Integer lineNumber) implements VariantLineChunk {
                 // Skipping the line really is the best solution, as the empty line is created by appending a line separator
                 // to the previous line. I added the additional if-statement, to only catch cases in which more than one line
                 // is out of bounds, which might indicate a problem.
-                Logger.debug(logMessage);
+                Logger.warn(logMessage);
             }
 
             return List.of();
