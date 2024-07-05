9a988ecd5cdefc8796797bc527d1f49df484f02e
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-07-05 10:33:55 +0200

    fix(dependencies): revert maven dependency install script

--- maven-scripts/build-local-repo.sh
+++ maven-scripts/build-local-repo.sh
@@ -1,8 +1,45 @@
 mvn install:install-file \
--DlocalRepositoryPath=../repo \
--Dfile=../src/main/resources/lib/functjonal-1.0-SNAPSHOT.jar \
--DgroupId=org.variantsync -DartifactId=functjonal-1.0-SNAPSHOT \
--Dversion=1.0-SNAPSHOT \
--Dpackaging=jar \
--DgeneratePom=true
+    -DlocalRepositoryPath=../repo \
+    -Dfile=../src/main/resources/lib/de.ovgu.featureide.lib.fm-v3.8.1.jar \
+    -DgroupId=de.ovgu \
+    -DartifactId=featureide.lib.fm \
+    -Dversion=3.8.1 \
+    -Dpackaging=jar \
+    -DgeneratePom=true
+
+mvn install:install-file \
+    -DlocalRepositoryPath=../repo \
+    -Dfile=../src/main/resources/lib/functjonal-1.0-SNAPSHOT.jar \
+    -DgroupId=org.variantsync \
+    -DartifactId=functjonal-1.0-SNAPSHOT \
+    -Dversion=1.0-SNAPSHOT \
+    -Dpackaging=jar \
+    -DgeneratePom=true
 rm -rf ~/.m2/repository/org/variantsync/
+
+mvn install:install-file \
+    -DlocalRepositoryPath=../repo \
+    -Dfile=../src/main/resources/lib/kernel_haven-1.0.0.jar \
+    -DgroupId=net.ssehub \
+    -DartifactId=kernel_haven \
+    -Dversion=1.0.0 \
+    -Dpackaging=jar \
+    -DgeneratePom=true
+
+mvn install:install-file \
+    -DlocalRepositoryPath=../repo \
+    -Dfile=../src/main/resources/lib/kconfigreader-1.0.0.jar \
+    -DgroupId=net.ssehub.kernelhaven \
+    -DartifactId=kconfigreader \
+    -Dversion=1.0.0 \
+    -Dpackaging=jar \
+    -DgeneratePom=true
+
+mvn install:install-file \
+    -DlocalRepositoryPath=../repo \
+    -Dfile=../src/main/resources/lib/org.sat4j.core.jar \
+    -DgroupId=org.sat4j \
+    -DartifactId=core \
+    -Dversion=2.3.5 \
+    -Dpackaging=jar \
+    -DgeneratePom=true
